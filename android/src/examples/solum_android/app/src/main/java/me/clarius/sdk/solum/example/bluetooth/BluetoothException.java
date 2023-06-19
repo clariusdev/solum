68cdafc100dc4bce44e5bf4b68184f3fe577d957
Julien Lemaitre <julien.lemaitre@clarius.com>
2023-06-19 09:42:40 -0700

    finish moving examples

--- android/src/examples/solum_android/app/src/main/java/me/clarius/sdk/solum/example/bluetooth/BluetoothException.java
+++
@@ -1,7 +0,0 @@
-package me.clarius.sdk.solum.example.bluetooth;
-
-public class BluetoothException extends Exception {
-    public BluetoothException(String message) {
-        super(message);
-    }
-}
