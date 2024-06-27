c6080a7875aa6147cbf30198bee865106cf07ada
Andreas Kleinöder <a.kleinoeder@dev-threads.de>
2024-06-27 11:24:09 +0200

    chore: add window dependencies for 11.3.0 version

--- include/solum/solum_export.h
+++ include/solum/solum_export.h
@@ -9,20 +9,20 @@
 #  ifndef SOLUM_EXPORT
 #    ifdef solum_EXPORTS
         /* We are building this library */
-#      define SOLUM_EXPORT __attribute__((visibility("default")))
+#      define SOLUM_EXPORT __declspec(dllexport)
 #    else
         /* We are using this library */
-#      define SOLUM_EXPORT __attribute__((visibility("default")))
+#      define SOLUM_EXPORT __declspec(dllimport)
 #    endif
 #  endif
 
 #  ifndef SOLUM_NO_EXPORT
-#    define SOLUM_NO_EXPORT __attribute__((visibility("hidden")))
+#    define SOLUM_NO_EXPORT 
 #  endif
 #endif
 
 #ifndef SOLUM_DEPRECATED
-#  define SOLUM_DEPRECATED __attribute__ ((__deprecated__))
+#  define SOLUM_DEPRECATED __declspec(deprecated)
 #endif
 
 #ifndef SOLUM_DEPRECATED_EXPORT
---
+++ include/solum/solum_export_winX64_11.3.0.h
@@ -0,0 +1,42 @@
+
+#ifndef SOLUM_EXPORT_H
+#define SOLUM_EXPORT_H
+
+#ifdef SOLUM_STATIC_DEFINE
+#  define SOLUM_EXPORT
+#  define SOLUM_NO_EXPORT
+#else
+#  ifndef SOLUM_EXPORT
+#    ifdef solum_EXPORTS
+        /* We are building this library */
+#      define SOLUM_EXPORT __declspec(dllexport)
+#    else
+        /* We are using this library */
+#      define SOLUM_EXPORT __declspec(dllimport)
+#    endif
+#  endif
+
+#  ifndef SOLUM_NO_EXPORT
+#    define SOLUM_NO_EXPORT 
+#  endif
+#endif
+
+#ifndef SOLUM_DEPRECATED
+#  define SOLUM_DEPRECATED __declspec(deprecated)
+#endif
+
+#ifndef SOLUM_DEPRECATED_EXPORT
+#  define SOLUM_DEPRECATED_EXPORT SOLUM_EXPORT SOLUM_DEPRECATED
+#endif
+
+#ifndef SOLUM_DEPRECATED_NO_EXPORT
+#  define SOLUM_DEPRECATED_NO_EXPORT SOLUM_NO_EXPORT SOLUM_DEPRECATED
+#endif
+
+#if 0 /* DEFINE_NO_DEPRECATED */
+#  ifndef SOLUM_NO_DEPRECATED
+#    define SOLUM_NO_DEPRECATED
+#  endif
+#endif
+
+#endif /* SOLUM_EXPORT_H */
